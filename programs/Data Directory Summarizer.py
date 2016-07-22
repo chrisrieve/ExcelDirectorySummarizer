59c5a0e8d919b6f3ac14a298dc8885f203f819c2
Rieve <christopher.rieve@owg.ds.corp>
2016-07-22 16:06:48 -0400

    Broke some stuff

--- programs/Data Directory Summarizer.py
+++ programs/Data Directory Summarizer.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 """
 Created on Thu Jul 14 15:22:58 2016
 
@@ -7,37 +7,42 @@ Created on Thu Jul 14 15:22:58 2016
 Goal: Create a python program template to grab all excel files and various
 	  information that may prove helpful
 ToDo:   1. Make paths relative
-		2.
+		2. Add number of columns
+Notes: worksheet.write(0, 0, "I'm sheet number %d" % (i + 1)) <-- Could come in
+	   handy later.
 """
+# pylint: disable=C0103
 
 import os
 import pandas as pd
 import sqlite3
 import readablebytes
+import xlsxwriter
 
 # Windows
 rootdir = r'C:\Users\christopher.rieve\NERA\Projects\Excel Directory Summarizer'
 # Mac
-# rootdir = '/Users/chrisrieve/Dropbox/File Cabinet/Python/Projects/ExcelDirectorySummarizer'
+# rootdir = '/Users/chrisrieve/Dropbox/File
+# Cabinet/Python/Projects/ExcelDirectorySummarizer'
 os.chdir(rootdir)
 # Mac
 # data_folder = rootdir + '/source'
 # Windows
-data_folder = rootdir + '\source'
+data_folder = rootdir + r'\source'
 data_files = []
 for subdir, dirs, files in os.walk(data_folder):
-	for file in files:
-		if file.endswith('.xls'):
-			data_files.append(os.path.join(subdir,file))
+    for f in files:
+        if f.endswith('.xls'):
+            data_files.append(os.path.join(subdir, f))
 
 column_names = [
-('Workbook', 'TEXT', 'Fill Name'),
-('# sheets', 'INTEGER', 0),
-('Sheetname', 'TEXT', 'Fill Sheet Name'),
-('rows', 'INTEGER', 0),
-('Workbook size', 'REAL', 0),
-('Useful(1-5)', 'INTEGER', 0),
-('description', 'TEXT', 'Insert Description')
+    ('Workbook', 'TEXT', 'Fill Name'),
+    ('# sheets', 'INTEGER', 0),
+    ('Sheetname', 'TEXT', 'Fill Sheet Name'),
+    ('rows', 'INTEGER', 0),
+    ('Workbook size', 'REAL', 0),
+    ('Useful(1-5)', 'INTEGER', 0),
+    ('description', 'TEXT', 'Insert Description')
 ]
 
 # sqlite experimental stuff
@@ -53,8 +58,8 @@ c = conn.cursor()
 # Dropping table if it exisits
 c.execute('DROP TABLE IF EXISTS excel_sheets')
 # Creating a new table in sqlite
-c.execute('CREATE TABLE {tn} ({nf} {ft})'\
-		.format (tn=table_name, nf=new_field, ft=field_type))
+c.execute('CREATE TABLE {tn} ({nf} {ft})'
+          .format(tn=table_name, nf=new_field, ft=field_type))
 conn.commit()
 conn.close()
 
@@ -64,48 +69,40 @@ c = conn.cursor()
 
 i = 0
 for col in column_names[1:]:
-	new_field = col[0]
-	field_type = col[1]
-	default_val = col[2]
-	c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"\
-			.format(tn=table_name, cn=new_field, ct=field_type, df=default_val))
+    new_field = col[0]
+    field_type = col[1]
+    default_val = col[2]
+    c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"
+              .format(tn=table_name, cn=new_field, ct=field_type, df=default_val))
 conn.commit()
 conn.close()
-for file in data_files[:1]:
-	xls = pd.ExcelFile(file)
-	# Time to add data to the table
-	conn = sqlite3.connect(sqlite_file)
-	c = conn.cursor()
-	for sheet in xls.sheet_names:
-		print sheet
-		try:
-			df = xls.parse(sheet)
-		except:
-			continue
-		sheet_rows = len(df.index)
-        print 'type of sheet rows', type(sheet_rows)
-        workbook = os.path.split(file)[1]
-        print 'type of workbook', type(workbook)
-        print 'workbook is', workbook
-        wb_size = readablebytes.humanize_bytes(os.path.getsize(file))
-        print 'type of wb size', type(wb_size)
+
+for f in data_files:
+    xls = pd.ExcelFile(f)
+    # Time to add data to the table
+    for sheet in xls.sheet_names:
+        conn = sqlite3.connect(sqlite_file)
+        c = conn.cursor()
+        try:
+            df = xls.parse(sheet)
+        except:
+            continue
+        sheet_rows = len(df.index)
+        workbook_name = os.path.split(f)[1]
+        wb_size = readablebytes.humanize_bytes(os.path.getsize(f))
         num_sheets = len(xls.sheet_names)
-        print 'type of num_sheets', type(num_sheets)
         sheet_name = sheet
-        print 'type of sheet name', type(sheet_name)
-        c.execute('''INSERT INTO excel_sheets('Workbook','# sheets','Sheetname','rows','Workbook size') VALUES(?,?,?,?,?)''',(workbook, num_sheets, sheet_name,sheet_rows, wb_size))
-
-
-conn.commit()
+        c.execute('''INSERT INTO excel_sheets('Workbook','# sheets',
+				  'Sheetname','rows','Workbook size') VALUES(?,?,?,?,?)''',
+                  (workbook_name, num_sheets, sheet_name, sheet_rows, wb_size))
 conn.close()
-'''
-column_names = [
-('Workbook', 'TEXT', 'Fill Name'),
-('# sheets', 'INTEGER', 0),
-('Sheetname', 'TEXT', 'Fill Sheet Name'),
-('rows', 'INTEGER', 0),
-('Workbook size', 'REAL', 0),
-('Useful(1-5)', 'INTEGER', 0),
-('description', 'TEXT', 'Insert Description')
-]
-'''
+conn.commit()
+
+# Exporting to excel file
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+workbook = xlsxwriter.Workbook('output/Summary of Files.xlsx')
+worksheet = workbook.add_worksheet('Data Summary')
+mysel = c.execute("SELECT * FROM excel_sheets")
+for row in mysel:
+    print row
