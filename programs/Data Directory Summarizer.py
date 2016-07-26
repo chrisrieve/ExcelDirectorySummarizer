e9c79afb91bcfc478f13ecbb868927e55d4852dd
Chris Rieve <chris.rieve2@gmail.com>
2016-07-26 18:01:58 -0400

    Changed method for find the number of rows and columns in an excel sheet. Added a lot of to dos. Also it has the first functioning form!

--- programs/Data Directory Summarizer.py
+++ programs/Data Directory Summarizer.py
@@ -5,30 +5,35 @@ Created on Thu Jul 14 15:22:58 2016
 @author: Christopher.Rieve
 
 Goal: Create a python program template to grab all excel files and various
-	  information that may prove helpful
-ToDo:   1. Make paths relative
-		2. Add number of columns
+      information that may prove helpful
+ToDo:   - Make paths relative
+        - Remove rows with zero columns and rows
+        - Make right click menu option
+        - Rewrite functionally
+        - Add column headers in excel sheet
 Notes: worksheet.write(0, 0, "I'm sheet number %d" % (i + 1)) <-- Could come in
-	   handy later.
+       handy later.
 """
 # pylint: disable=C0103
-
+# pylint: disable=W0312
 
 import os
 import sqlite3
-import pandas as pd
+
 import readablebytes
+import xlrd
 import xlsxwriter
 
 # Windows
-# rootdir = r'C:\Users\christopher.rieve\NERA\Projects\Excel Directory Summarizer'
+rootdir = r'C:\Users\christopher.rieve\NERA\Projects\Excel Directory Summarizer'
 # Mac
-rootdir = '/Users/chrisrieve/Dropbox/File Cabinet/Python/Projects/ExcelDirectorySummarizer'
+# rootdir = '/Users/chrisrieve/Dropbox/File
+# Cabinet/Python/Projects/ExcelDirectorySummarizer'
 os.chdir(rootdir)
 # Mac
-data_folder = rootdir + '/source'
+# data_folder = rootdir + '/source'
 # Windows
-# data_folder = rootdir + r'\source'
+data_folder = rootdir + r'\source'
 data_files = []
 for subdir, dirs, files in os.walk(data_folder):
     for f in files:
@@ -39,10 +44,11 @@ column_names = [
     ('Workbook', 'TEXT', 'Fill Name'),
     ('# sheets', 'INTEGER', 0),
     ('Sheetname', 'TEXT', 'Fill Sheet Name'),
-    ('rows', 'INTEGER', 0),
-    ('Workbook size', 'REAL', 0),
+    ('Rows', 'INTEGER', 0),
+    ('Columns', 'INTEGER', 0),
+    ('Workbook Size', 'REAL', 0),
     ('Useful(1-5)', 'INTEGER', 0),
-    ('description', 'TEXT', 'Insert Description')
+    ('Description', 'TEXT', 'Insert Description')
 ]
 
 # sqlite experimental stuff
@@ -67,7 +73,6 @@ conn.close()
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
-i = 0
 for col in column_names[1:]:
     new_field = col[0]
     field_type = col[1]
@@ -78,25 +83,27 @@ conn.commit()
 conn.close()
 
 for f in data_files:
-    xls = pd.ExcelFile(f)
+    print f
+    workbook = xlrd.open_workbook(f)
+    sheet_names = workbook.sheet_names()
     # Time to add data to the table
-    for sheet in xls.sheet_names:
+    for sheet in sheet_names:
+        print sheet
         conn = sqlite3.connect(sqlite_file)
         c = conn.cursor()
-        try:
-            excel_frame = xls.parse(sheet)
-        except ValueError as err:
-            print err.args
-        sheet_rows = len(excel_frame.index)
+        sheet_object = workbook.sheet_by_name(sheet)
+        sheet_rows = sheet_object.nrows
+        sheet_cols = sheet_object.ncols
         workbook_name = os.path.split(f)[1]
         wb_size = readablebytes.humanize_bytes(os.path.getsize(f))
-        num_sheets = len(xls.sheet_names)
+        num_sheets = len(sheet_names)
         sheet_name = sheet
         c.execute('''INSERT INTO excel_sheets('Workbook','# sheets',
-				  'Sheetname','rows','Workbook size') VALUES(?,?,?,?,?)''',
-                  (workbook_name, num_sheets, sheet_name, sheet_rows, wb_size))
-conn.close()
-conn.commit()
+                   'Sheetname','Rows','Columns','Workbook Size') VALUES(?,?,?,?,?,?)''',
+                  (workbook_name, num_sheets, sheet_name, sheet_rows, sheet_cols, wb_size))
+        conn.commit()
+        conn.close()
+
 
 # Exporting to excel file
 conn = sqlite3.connect(sqlite_file)
@@ -104,5 +111,8 @@ c = conn.cursor()
 workbook = xlsxwriter.Workbook('output/Summary of Files.xlsx')
 worksheet = workbook.add_worksheet('Data Summary')
 mysel = c.execute("SELECT * FROM excel_sheets")
-for row in mysel:
+for i, row in enumerate(mysel):
     print row
+    for j, value in enumerate(row):
+        worksheet.write(i, j, value)
+workbook.close()
